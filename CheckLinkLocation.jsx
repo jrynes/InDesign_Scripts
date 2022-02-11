89ebd4cf07bd078eefa514a13585119a058bd90c
Jeff Rynes <jrynes3@gmail.com>
2022-02-11 14:28:15 +0100

    Added Javascript XML (JSX) file for checking InDesign link locations
    InDesign Script to check link locations

---
+++ CheckLinkLocation.jsx
@@ -0,0 +1,46 @@
+//Check link locations to ensure that all links are in the correct folder
+
+LinkCheck();
+
+function LinkCheck() {
+    
+      var sourceDoc = app.activeDocument;
+      var mLinks = sourceDoc.links.everyItem().getElements();  
+      var destFolder = "~/Desktop/copiedLinks/";  
+      var currLinkFName, currFile;  
+      var len = mLinks.length;
+      
+      destFolder = Folder.selectDialog("Choose target folder to save the log text files") + "/";  
+      
+     //If the target folder for the log file does not exist, create it 
+    if ( !Folder(destFolder).create() ) exit(); 
+    
+    //Check if Log File Already exists
+    
+    //If the log file does not exist, then create a new file
+    var textFile = new File(destFolder + "/" + sourceDoc.name + " - LogFile.txt");
+     
+    textFile.open("w");
+    textFile.writeln(sourceDoc.name + "\r\n")
+      
+    //Iterate through links in document
+    while (len-->0) {
+      //Get file path of current link
+      currLinkFName = mLinks[len].filePath;
+      var errorFlag = ""
+      if (currLinkFName.indexOf('500-Draft Charrette Report') === -1 ) {
+          errorFlag = "ERROR - CHECK LINK --> "
+          }
+      textFile.writeln(errorFlag + currLinkFName)
+      //Append link to text file in target folder
+      
+      //currFile = File( destFolder + File(currLinkFName).name );  
+      //if ( !currFile.exists && File(currLinkFName).exists )   
+      //mLinks[len].copyLink(currFile);  
+      }
+  
+       textFile.close();
+    
+    
+    }
+
